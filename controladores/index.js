0ed2ef5f5f6db9fdad7e4e33196ae19e40cc5e39
joaociola <joaolucasciola1@gmail.com>
2023-09-28 23:30:36 -0300

    estilizado

--- controladores/index.js
+++ controladores/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const axios = require('axios');
+// const express = require('express');
+// const axios = require('axios');
 
-const app = express();
+// const app = express();
 
-const instance = axios.create({
-    baseURL: 'https://viacep.com.br/ws/',
-});
+// const instance = axios.create({
+//     baseURL: 'https://viacep.com.br/ws/',
+// });
 
-app.get('/:cep', async (req, res) => {
-    const { cep } = req.params;
+// app.get('/:cep', async (req, res) => {
+//     const { cep } = req.params;
 
-    try {
-        const resultado = await instance.get(`${cep}/json`);
-        console.log(resultado.data);
-        res.json(resultado.data);
-    } catch (error) {
-        res.status(500).json({ error: 'Ocorreu um erro ao buscar o CEP.' });
-    }
-});
+//     try {
+//         const resultado = await instance.get(`${cep}/json`);
+//         console.log(resultado.data);
+//         res.json(resultado.data);
+//     } catch (error) {
+//         res.status(500).json({ error: 'Ocorreu um erro ao buscar o CEP.' });
+//     }
+// });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Servidor rodando na porta 3000');
+// });
